8b4ae8a287e1959158d9c08f6f940e881cfce96e
Habibur Rahman <habibur.rahman0927@gmail.com>
2021-08-09 05:43:38 -0700

    vanilla rest api

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const { getProducts, getProduct, createProduct,updateProduct, deleteProduct } = require('./controllers/productController');
+const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
+    // console.log(123);
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/html');
+    // res.write('<h1>Hello World</h1>');
+    // res.end();
+
+    if (req.url === '/api/products' && req.method === 'GET') {
+        getProducts(req, res);
+    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
+        const id = req.url.split('/')[3];
+        getProduct(req, res, id)
+    } else if (req.url === '/api/products' && req.method === 'POST') {
+        createProduct(req, res);
+    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
+        const id = req.url.split('/')[3];
+        updateProduct(req, res, id);
+    }else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
+        const id = req.url.split('/')[3];
+        deleteProduct(req, res, id);
+    }else {
+        res.writeHead(404, { 'Content-Type': 'text/json' });
+        res.end(JSON.stringify({ message: "Route Not Found" }));
+    }
+
+
+
+});
+
+server.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
